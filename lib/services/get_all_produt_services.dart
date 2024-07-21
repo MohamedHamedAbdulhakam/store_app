a895e0da02de5350c643a17da6728c7d39d2c0de
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-21 22:53:10 +0300

    all product services

--- lib/services/get_all_produt_services.dart
+++ lib/services/get_all_produt_services.dart
@@ -1,1 +1,17 @@
-class GetAllProdutServices {}
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:store_app/models/produt_model.dart';
+
+class GetAllProdutServices {
+  Future<List<ProdutModel>> getALLProducts() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://fakestoreapi.com/products/category/:category_name?='));
+    List<dynamic> data = jsonDecode(response.body);
+    List<ProdutModel> productList = [];
+    for (int i = 0; i < data.length; i++) {
+      productList.add(ProdutModel.fromJson(data[i]));
+    }
+    return productList;
+  }
+}
