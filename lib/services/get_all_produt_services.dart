58e04bf77dfede5830399b2f347eccc74085d114
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-07-22 18:28:20 +0300

    status code

--- lib/services/get_all_produt_services.dart
+++ lib/services/get_all_produt_services.dart
@@ -7,11 +7,17 @@ class GetAllProdutServices {
   Future<List<ProductModel>> getALLProducts() async {
     http.Response response = await http.get(Uri.parse(
         'https://fakestoreapi.com/products/category/:category_name?='));
-    List<dynamic> data = jsonDecode(response.body);
-    List<ProductModel> productList = [];
-    for (int i = 0; i < data.length; i++) {
-      productList.add(ProductModel.fromJson(data[i]));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      List<ProductModel> productList = [];
+      for (int i = 0; i < data.length; i++) {
+        productList.add(ProductModel.fromJson(data[i]));
+      }
+      return productList;
+    } else {
+      throw Exception(
+          'there is a problem with status code ${response.statusCode}');
     }
-    return productList;
   }
 }
