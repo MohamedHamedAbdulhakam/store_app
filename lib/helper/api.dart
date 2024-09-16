303f3fbab0977653e138414f11574c4614a3ab5e
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-09-16 12:08:27 +0300

    add product service

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -24,7 +24,12 @@ class Api {
     }
     http.Response response =
         await http.post(Uri.parse(urL), body: body, headers: headers);
-    Map<String, dynamic> data = jsonDecode(response.body);
-    return data;
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          "there was a problem with status code ${response.statusCode}with body ${response.body}");
+    }
   }
 }
--- lib/models/produt_model.dart
+++ lib/models/produt_model.dart
@@ -1,7 +1,7 @@
 class ProductModel {
   final int id;
   final String title;
-  final double prince;
+  final double price;
   final String description;
   final String image;
   final RatingModel rating;
@@ -9,7 +9,7 @@ class ProductModel {
   ProductModel({
     required this.id,
     required this.title,
-    required this.prince,
+    required this.price,
     required this.description,
     required this.image,
     required this.rating,
@@ -19,7 +19,7 @@ class ProductModel {
     return ProductModel(
         id: jsonData['id'],
         title: jsonData['title'],
-        prince: jsonData['price'],
+        price: jsonData['price'],
         description: jsonData['description'],
         image: jsonData['image'],
         rating: RatingModel.fromJson(jsonData('rating')));//map of rating
