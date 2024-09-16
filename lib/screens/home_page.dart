54562c7d65b701fdc4bfbfccdc4da3535e289729
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-09-16 21:37:59 +0300

    stack and positioned widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -24,39 +24,50 @@ class HomePage extends StatelessWidget {
           )),
         )),
       ),
-      body: Container(
-        width: 200,
-        height: 120,
-        child: const Card(
-          color: Colors.white,
-          elevation: 6,
-          shape: BeveledRectangleBorder(),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'Handbag',
-                  style: TextStyle(color: Colors.grey),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Center(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              width: 200,
+              height: 120,
+              child:  Card(
+                color: Colors.white,
+                elevation: 6,
+                shape: const BeveledRectangleBorder(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Text(r'$33'),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                      )
-                    ],
-                  ),
-                )
-              ],
+                      const Text(
+                        'Handbag',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(r'$33'),
+                            Icon(
+                              Icons.favorite,
+                              color: Colors.red,
+                            )
+                          ],
+                        ),
+                      ),
+                        
+                    ]),
+                ),
+              ),
             ),
-          ),
+         Positioned(
+                      left: 95,
+                      bottom: 65,
+            child: Image.network('https://up.yimg.com/ib/th?id=OIP.M56Z_XU7r_UeDFR05bfkzwHaE7&pid=Api&rs=1&c=1&qlt=95&w=152&h=101',height: 100,width: 100,))
+          ],
         ),
       ),
     );
