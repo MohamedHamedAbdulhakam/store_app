5a1e4a5be4a3a4cd082880f09fc280a495d55c31
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-09-16 12:30:28 +0300

    make get request reusable

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -3,54 +3,58 @@ import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<dynamic> get({required String url}) async {
-    http.Response response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception(
-          'there is a problem with status code ${response.statusCode}');
-    }
-  }
-
-  Future<dynamic> post({
-    required String urL,
-    @required dynamic body,
-    @required String? taken,
-  }) async {
+  Future<dynamic> get({required String url, @required String? taken}) async {
     Map<String, String> headers = {};
     if (taken != null) {
       headers.addAll({'Authorization': 'Bearer $taken'});
+      http.Response response = await http.get(Uri.parse(url), headers: headers);
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        throw Exception(
+            'there is a problem with status code ${response.statusCode}');
+      }
     }
-    http.Response response =
-        await http.post(Uri.parse(urL), body: body, headers: headers);
-    if (response.statusCode == 200) {
-      Map<String, dynamic> data = jsonDecode(response.body);
-      return data;
-    } else {
-      throw Exception(
-          "there was a problem with status code ${response.statusCode}with body ${response.body}");
-    }
-  }
 
-  Future<dynamic> put({
-    required String urL,
-    @required dynamic body,
-    @required String? taken,
-  }) async {
-    Map<String, String> headers = {};
-    headers.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
-    if (taken != null) {
-      headers.addAll({'Authorization': 'Bearer $taken'});
+    Future<dynamic> post({
+      required String urL,
+      @required dynamic body,
+      @required String? taken,
+    }) async {
+      Map<String, String> headers = {};
+      if (taken != null) {
+        headers.addAll({'Authorization': 'Bearer $taken'});
+      }
+      http.Response response =
+          await http.post(Uri.parse(urL), body: body, headers: headers);
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = jsonDecode(response.body);
+        return data;
+      } else {
+        throw Exception(
+            "there was a problem with status code ${response.statusCode}with body ${response.body}");
+      }
     }
-    http.Response response =
-        await http.post(Uri.parse(urL), body: body, headers: headers);
-    if (response.statusCode == 200) {
-      Map<String, dynamic> data = jsonDecode(response.body);
-      return data;
-    } else {
-      throw Exception(
-          "there was a problem with status code ${response.statusCode}with body ${response.body}");
+
+    Future<dynamic> put({
+      required String urL,
+      @required dynamic body,
+      @required String? taken,
+    }) async {
+      Map<String, String> headers = {};
+      headers.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
+      if (taken != null) {
+        headers.addAll({'Authorization': 'Bearer $taken'});
+      }
+      http.Response response =
+          await http.post(Uri.parse(urL), body: body, headers: headers);
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = jsonDecode(response.body);
+        return data;
+      } else {
+        throw Exception(
+            "there was a problem with status code ${response.statusCode}with body ${response.body}");
+      }
     }
   }
 }
